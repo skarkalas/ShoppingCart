176e0c362965b8cc2b166ee12d3dbd8e4e83defe
sokratis.karkalas <sokratis.karkalas@gmail.com>
2016-06-21 10:15:53 +0100

    part 1 is done

--- Shop.scala
+++ Shop.scala
@@ -1,4 +1,26 @@
+abstract class Merchandise
+class Apple extends Merchandise
+class Orange extends Merchandise
+
 class Shop
 {
-	def checkout(goods: List[String]) = 0
+	def checkout(goods: List[Merchandise]): Int = 
+	{
+		def matchApples(good: Merchandise) = good match
+		{
+			case apple: Apple => true
+			case _ => false
+		}
+
+		def matchOranges(good: Merchandise) = good match
+		{
+			case orange: Orange => true
+			case _ => false
+		}
+		
+		val apples = goods.filter(matchApples).length
+		var oranges = goods.filter(matchOranges).length
+		
+		apples * 60 + oranges * 25
+	}
 }
--- ShopTest.scala
+++ ShopTest.scala
@@ -18,8 +18,8 @@ class ShopTest extends TestCase
 	
 	def testCheckout()
 	{
-		val goods = List[String]("test")
-		assertEquals("empty", 0, shop.checkout(goods))
+		val goods = List[Merchandise](new Apple, new Apple, new Apple, new Orange, new Orange)
+		assertEquals("3 x 60 + 2 x 25 must be 230", 230, shop.checkout(goods))
 	}
 }
 
